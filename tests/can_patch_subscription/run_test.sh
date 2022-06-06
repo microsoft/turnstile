3b1c936896f108509d604582ecb543cc00ea6a6f
Casey Watson <cawatson@microsoft.com>
2022-06-06 16:45:41 -0500

    Really want to get this ticking.

--- tests/can_patch_subscription/run_test.sh
+++ tests/can_patch_subscription/run_test.sh
@@ -59,15 +59,15 @@ if [[ $post_status_code == "200" ]]; then
             -H "x-functions-key: $api_key" \
             "$url")
 
-        projector="{subscription_id, subscription_name, tenant_id, tenant_name, offer_id, plan_id, state, admin_role_name, user_role_name, admin_name, admin_email, total_seats, is_being_configred, is_free_trial, is_setup_complete, is_test_subscription, subscriber_info, source_subscription, seating_config.seating_strategy_name, seating_config.limited_overflow_seating_enabled, seating_config.seat_reservation_expiry_in_days, seating_config.default_seat_expiry_in_days}"
+        projector="{subscription_id, subscription_name, tenant_id, tenant_name, offer_id, plan_id, state, admin_role_name, user_role_name, admin_name, admin_email, total_seats, is_being_configred, is_free_trial, is_setup_complete, is_test_subscription, subscriber_country: .subscriber_info.country, source_subscription_id: .source_subscription.id, seating_strategy_name: .seating_config.seating_strategy_name, limited_overflow_seating: .seating_config.limited_overflow_seating_enabled, reservation_expiry: .seating_config.seat_reservation_expiry_in_days, seat_expiry: .seating_config.default_seat_expiry_in_days}"
 
         actual_subscription=$(echo "$get_response" | jq "$projector")
         expected_subscription=$(echo "$patch_json" | jq "$projector")
 
         if [[ "$actual_subscription" == "$expected_subscription" ]]; then
-            echo "✔️   Can create subscription test passed."
+            echo "✔️   Can patch subscription; test passed."
         else
-            echo "❌   Can create subscription test failed."
+            echo "❌   Can't patch subscription; test failed."
             echo
             echo "Expected subscription is..."
             echo $(echo "$expected_subscription" | jq ".")
