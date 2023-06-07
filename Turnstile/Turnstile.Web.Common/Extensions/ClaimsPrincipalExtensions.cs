e94194e19a1bc0440cb5260887670ab8215941a5
Casey Watson <cawatson@microsoft.com>
2023-06-07 09:41:27 -0500

    Fixed "on screen" bug

---
+++ Turnstile/Turnstile.Web.Common/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Turnstile.Web.Common.Extensions
+{
+    internal class ClaimsPrincipalExtensions
+    {
+    }
+}
